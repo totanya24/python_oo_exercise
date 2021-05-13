e69d7f260013f0159994dd723e535f02ff966847
Tanya Durisheti <totanya24@gmail.com>
2021-05-13 18:33:28 -0400

    initial

---
+++ serial.py
@@ -0,0 +1,38 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
+    def __init__(self, start=0):
+        self.start = self.next = start
+
+
+    def generate(self):
+
+        self.next += 1
+        return self.next
+
+    def __repr__(self):
+        return f"<SerialGenerator start ={self.start} next{self.next}>"
+
+
+    def reset(self):
+         self.next = self.start
+
---
+++ wordfinder.py
@@ -0,0 +1,20 @@
+"""Word Finder: finds random words from a dictionary."""
+import random
+
+class WordFinder:
+    ...
+
+    def __init__(self, path):
+        dict_file = open(path)
+        self.words = self.parse(dict_file)
+        print(f"{len(self.words)} word read")
+
+    def parse(self, dict_file):
+        return [w.strip() for w in dict_file]
+    def random(self):
+        return random.choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    def parse(self, dict_file):
+        return [w.srip() for w in dict_file
+                if w.strip() and not w.startswith('#')]
